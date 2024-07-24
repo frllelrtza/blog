404b7efa2ce3e7aaafe69cd6253167ad58c5f4bc
frllelrtza <alfarrelelmaretza@gmail.com>
2024-07-24 23:15:56 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>>
+    <h1>Halaman About</h1>
+    <img src="assets/img/frl.png" alt="Farrel" width="200px">
+
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
