01ce7a503923602a19ca6cb5b223868e27fc19af
frllelrtza <alfarrelelmaretza@gmail.com>
2024-07-27 01:22:14 +0700

    Learn Artisan Tinker

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,26 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Contact</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-    <h1>Halaman Blog</h1>
-    <article>
-        <h3>Judul</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, dicta. elit. Accusantium, dicta. adipisicing elit. Accusantium, dicta elit. Accusantium, dicta. adipisicing elit. Accusantium, dicta</p>
-    </article> 
-
-    <article>
-        <h3>Judul</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, dicta. elit. Accusantium, dicta. adipisicing elit. Accusantium, dicta elit. Accusantium, dicta. adipisicing elit. Accusantium, dicta</p>
-    </article> 
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Ini adalah halaman Contact</h3>
+  </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,29 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about');
+    return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{post:slug}', function(Post $post){
+    
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
